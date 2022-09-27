a1828a0d995eea2a434be962c36d91ba2910d6ff
Dani Ihza Farrosi <dani.videda@gmail.com>
2022-09-27 18:38:44 +0700

    setup middleware and error handling

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 3000
+const { errorHandler } = require('./middlewares/errorMiddleware')
 
 const app = express()
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: false }))
+
+app.use(
+    '/api/goals', // route
+    express.urlencoded({ extended: false }), // middleware to parse the urlencoded payload
+    require('./routes/goalRoutes'), // router that consist of different api.METODS
+    errorHandler // middleware to handle error
+)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
