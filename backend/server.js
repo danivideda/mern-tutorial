95499ff3a40271252cf74f807fab8ee8037ab04d
Dani Ihza Farrosi <dani.videda@gmail.com>
2022-09-27 10:11:12 +0700

    set routes with express.Router

--- backend/server.js
+++ backend/server.js
@@ -3,4 +3,7 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT || 3000
 
 const app = express()
+
+app.use('/api/goals', require('./routes/goalsRoutes'))
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
