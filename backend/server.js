efc872ae8a216a25ff390ccdd03ad05f2255a2d6
Dani Ihza Farrosi <dani.videda@gmail.com>
2022-09-27 08:28:58 +0700

    setup basic server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,6 @@
-console.log('Hello world')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 3000
+
+const app = express()
+app.listen(port, () => console.log(`Server started on port ${port}`))
