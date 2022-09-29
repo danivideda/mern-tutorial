28fb5f04e74fad6ca36d15cc9bd1df9dd4fd5a31
Dani Ihza Farrosi <dani.videda@gmail.com>
2022-09-29 07:57:45 +0700

    add async handler (2)

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,9 +3,9 @@ const asyncHandler = require('express-async-handler')
 // @desc    Get goals
 // @route   GET /api/goals/
 // @access  Private
-const getGoals = async (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get goals' })
-}
+})
 
 // @desc    Set goal
 // @route   SET /api/goals/
