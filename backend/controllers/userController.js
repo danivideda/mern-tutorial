0bae57e284b8e6482f92f0c1de630edbecaa1704
Dani Ihza Farrosi <dani.videda@gmail.com>
2022-10-04 11:31:51 +0700

    login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,9 +49,25 @@ const registerUser = asyncHandler(async (req, res) => {
 // @desc    Login user
 // @route   POST /api/users/
 // @access  Public
-const loginUser = (req, res) => {
-    res.status(200).json({ message: 'Login User' })
-}
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+
+    // Check for user email and password
+    const user = await User.findOne({ email })
+    if (user && await bcrypt.compare(password, user.password)) {
+        res.status(200).json({
+            message: 'Login user successfull',
+            data: {
+                _id: user.id,
+                name: user.name,
+                email: user.email
+            }
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
+})
 
 // @desc    Get user
 // @route   GET /api/users/
